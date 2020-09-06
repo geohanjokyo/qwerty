0d665e889615b3a1689541a8c4e64b4e26748241
geohanjokyo <kimsuhan@hotmail.com>
2020-09-06 12:18:06 +0900

    최종

--- kbd.py
+++ kbd.py
@@ -3,7 +3,6 @@ import os
 import pandas as pd
 from appium import webdriver
 from time import sleep
-
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
